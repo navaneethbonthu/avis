239aa2df9b879fcf62d564045897f58265a1a45e
navaneethbonthu <navaneethbonthu@gmail.com>
2025-01-28 17:55:42 +0530

    bootstrap and jquery links added

--- src/app/pages/dash-board/dash-board.component.ts
+++ src/app/pages/dash-board/dash-board.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-
+import { ButtonModule } from 'primeng/button';
+declare var $: any;
 @Component({
   selector: 'app-dash-board',
-  imports: [],
+  imports: [ButtonModule],
+  standalone: true,
   templateUrl: './dash-board.component.html',
   styleUrl: './dash-board.component.scss'
 })
 export default class DashBoardComponent {
-
+  ngOnInit() {
+    
+  }
 }
