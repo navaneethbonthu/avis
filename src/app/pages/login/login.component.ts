28bcb07c22101e5e15d58e234565cba44439dfa1
navaneethbonthu <navaneethbonthu@gmail.com>
2025-01-30 17:04:07 +0530

    login form added

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html'
 })
 export default class LoginComponent {
+
+  private readonly fb = inject(FormBuilder)
+  userLoginForm = this.fb.nonNullable.group({
+    username: ['', Validators.required,Validators.minLength(3), Validators.maxLength(20)],
+    user_password: ['', Validators.required, Validators.minLength(6), Validators.maxLength(20)],
+  })
+
+  onSubmit(){
+    
+  }
 
 }
