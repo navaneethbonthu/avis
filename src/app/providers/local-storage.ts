748a613bb844b0ae85086ca2efbb57c5a4c3e0af
navaneethbonthu <navaneethbonthu@gmail.com>
2025-02-19 16:48:52 +0530

    updated

---
+++ src/app/providers/local-storage.ts
@@ -0,0 +1,56 @@
+import { DOCUMENT } from "@angular/common";
+import { inject, Injectable, InjectionToken } from "@angular/core";
+
+
+export const LOCAL_STORAGE = new InjectionToken<Storage | null>('Local Storage', {
+
+    factory:() => {
+        const document = inject(DOCUMENT).defaultView;
+        return  document?.localStorage ?? null
+    }
+
+});
+
+@Injectable({
+    providedIn: 'root'
+})
+export class LocalStorage{
+
+    private readonly storage = inject(LOCAL_STORAGE);
+
+
+    public set(key: string, value: string | object){
+ 
+        if(!this.storage) return;
+
+        if(typeof value === 'object'){
+            this.storage.setItem(key, JSON.stringify(value));
+        } else {
+            this.storage.setItem(key, value);
+        }
+    }
+
+    public get<TData = string>(key: string):any{
+        if(!this.storage) return null;
+
+        const data = this.storage.getItem(key);
+
+        if(!data) return null;
+
+        try{
+            const parsedData = JSON.parse(data);
+            if(typeof parsedData === 'object'){
+                return parsedData as TData extends object ? TData : string;
+            }
+        }catch(err) {
+            return data as TData extends object ? TData : string
+        }
+       
+    }
+
+
+   
+
+
+
+}
